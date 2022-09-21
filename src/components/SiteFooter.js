78542e2fca76bab50aa72399a4ff0b322d17c350
Max Kohler <hello@maxkohler.com>
2022-09-21 11:37:00 +0200

    Iterate design

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -1,12 +1,31 @@
 import React from "react";
-import * as styles from "./SiteFooter.module.scss"
+import * as styles from "./SiteFooter.module.scss";
 
 export default function SiteFooter() {
   return (
     <footer className={styles.container}>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <a href="#1">EUNPDC</a>
+          </li>
+          <li>
+            <a href="#1">PRIF</a>
+          </li>
+          <li>
+            <a href="#1">Terms</a>
+          </li>
+          <li>
+            <a href="#1">Privacy</a>
+          </li>
+          <li>
+            <a href="#1">Contact</a>
+          </li>
+        </ul>
+      </nav>
       <p>
-        This course aims to cover all aspects of the EU non-proliferation and disarmament agenda and provide a comprehensive knowledge resource. Watch
-        the trailer for a quick overview or jump right in!
+        The EU Non-Proliferation and Disarmament eLearning Course aims to cover all aspects of the EU non-proliferation and disarmament agenda. It's
+        produced by PRIF with financial assistance of the European Union. The contents of individual learning units are the sole responsibility of the respective authors and don't necessariy reflect the position of the European Union.
       </p>
     </footer>
   );
