18697b9df8b95f7bf2c74f39630c4998de7a0391
Max Kohler <hello@maxkohler.com>
2022-12-05 14:42:34 +0100

    Solve hydration issue!

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,19 +1,23 @@
 import React, { cloneElement } from "react";
 import { useState } from "react";
+import { useId } from "react";
 import * as styles from "./Tabs.module.scss";
 
-function Tab({ children, tabId, labelledBy, isActive, key }) {
+function Tab({ children, tabId, labelledBy, isActive }) {
   const activeClass = isActive ? styles.tabContentActive : "";
   return (
-    <div>This is a test tab element</div>
-    // <div key={key} className={`${styles.tabContent} ${activeClass}`} aria-labelledby={labelledBy}>
-    //   {children}
-    // </div>
+    <div className={`${styles.tabContent} ${activeClass}`} aria-labelledby={labelledBy} id={tabId}>
+      {children}
+    </div>
   );
 }
 
 function Tabs({ children }) {
+  const baseId = useId();
   const [currentTab, setCurrentTab] = useState(0);
+  function generateId(base, index) {
+    return `${base}-${index}`;
+  }
 
   function handleTabClick(e, i) {
     e.preventDefault();
@@ -21,35 +25,31 @@ function Tabs({ children }) {
   }
 
   const filteredChildren = React.Children.toArray(children).filter((c) => c.type);
-  console.log(`Children`)
-  console.log(children)
-  console.log(`filteredChildren`)
-  console.log(filteredChildren)
   const tabItems = filteredChildren.map((c, i) => {
+    const id = generateId(baseId, i);
     const isActive = currentTab === i;
-    // const activeClass = isActive ? styles.tabActive : "";
-    // const title = c.props.title;
-      // <li key={`tabs-${i}`} className={`${styles.tab} ${activeClass}`}>
-      //   <button type="button" onClick={(e) => handleTabClick(e, i)} role="tab">
-      //     {title}
-      //   </button>
-      // </li>
+    const activeClass = isActive ? styles.tabActive : "";
+    const title = c.props.title;
     return (
-      <li key={`tabs-${i}`}>Tabs Item</li>
+      <li key={`tabs-${id}-${i}`} className={`${styles.tab} ${activeClass}`}>
+        <button type="button" onClick={(e) => handleTabClick(e, i)} aria-controls={id} role="tab">
+          {title}
+        </button>
+      </li>
     );
   });
-  // const tabContent = filteredChildren.map((c, i) => {
-  //   const props = {
-  //     isActive: currentTab === i,
-  //     key: `tabcontent-${i}`,
-  //   };
-  //   return(<div>This is a test element (where the cloned tab should be)</div>)
-  //   // return cloneElement(c, props);
-  // });
-  const tabContent = <>This is tabContent</>;
+  const tabContent = filteredChildren.map((c, i) => {
+    const props = {
+      tabId: generateId(baseId, i),
+      isActive: currentTab === i,
+    };
+    return cloneElement(c, props);
+  });
   return (
     <div className={styles.container}>
-      <ul className={styles.tabsList}>{tabItems && tabItems}</ul>
+      <ul className={styles.tabsList} role="tablist">
+        {tabItems}
+      </ul>
       <div>{tabContent}</div>
     </div>
   );
