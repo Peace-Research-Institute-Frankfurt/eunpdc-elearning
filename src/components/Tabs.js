89e5743bba969534703ea80b72f9124ffccd29ea
Max Kohler <hello@maxkohler.com>
2022-12-05 14:09:08 +0100

    Debug hydration error

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -13,34 +13,35 @@ function Tab({ children, tabId, labelledBy, isActive, key }) {
 }
 
 function Tabs({ children }) {
-  const [currentTab, setCurrentTab] = useState(0);
+  // const [currentTab, setCurrentTab] = useState(0);
 
-  function handleTabClick(e, i) {
-    e.preventDefault();
-    setCurrentTab(i);
-  }
+  // function handleTabClick(e, i) {
+  //   e.preventDefault();
+  //   setCurrentTab(i);
+  // }
 
-  const filteredChildren = React.Children.toArray(children).filter((c) => c.type && c.type.name === "Tab");
-  const tabItems = filteredChildren.map((c, i) => {
-    const isActive = currentTab === i;
-    const activeClass = isActive ? styles.tabActive : "";
-    const title = c.props.title;
-    return (
-      <li key={`tabs-${i}`} className={`${styles.tab} ${activeClass}`}>
-        <button type="button" onClick={(e) => handleTabClick(e, i)} role="tab">
-          {title}
-        </button>
-      </li>
-    );
-  });
-  const tabContent = filteredChildren.map((c, i) => {
-    const props = {
-      isActive: currentTab === i,
-      key: `tabcontent-${i}`,
-    };
-    return(<div>This is a test element (where the cloned tab should be)</div>)
-    // return cloneElement(c, props);
-  });
+  // const filteredChildren = React.Children.toArray(children).filter((c) => c.type && c.type.name === "Tab");
+  // const tabItems = filteredChildren.map((c, i) => {
+  //   const isActive = currentTab === i;
+  //   const activeClass = isActive ? styles.tabActive : "";
+  //   const title = c.props.title;
+  //   return (
+  //     <li key={`tabs-${i}`} className={`${styles.tab} ${activeClass}`}>
+  //       <button type="button" onClick={(e) => handleTabClick(e, i)} role="tab">
+  //         {title}
+  //       </button>
+  //     </li>
+  //   );
+  // });
+  // const tabContent = filteredChildren.map((c, i) => {
+  //   const props = {
+  //     isActive: currentTab === i,
+  //     key: `tabcontent-${i}`,
+  //   };
+  //   return(<div>This is a test element (where the cloned tab should be)</div>)
+  //   // return cloneElement(c, props);
+  // });
+  const tabContent = <>This is tabContent</>
   return (
     <div className={styles.container}>
       <div>{tabContent}</div>
