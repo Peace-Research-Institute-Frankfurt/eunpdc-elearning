539e539bfb3566a3f07e0a5809aeee52c2843a6c
Max Kohler <hello@maxkohler.com>
2022-12-05 12:29:50 +0100

    Fix invalid props

--- src/components/Embed.js
+++ src/components/Embed.js
@@ -3,9 +3,9 @@ import * as styles from "./Embed.module.scss";
 
 function Embed({ src, size, caption }) {
   return (
-    <figure class={`${styles.container} ${styles[size]}`}>
-      <div class={styles.iframeContainer}>
-        <iframe src={src} loading="lazy" allowFullScreen frameborder="0"></iframe>
+    <figure className={`${styles.container} ${styles[size]}`}>
+      <div className={styles.iframeContainer}>
+        <iframe src={src} loading="lazy" allowFullScreen frameBorder="0"></iframe>
       </div>
       {caption !== "" && <figcaption className={styles.caption}>{caption}</figcaption>}
     </figure>
