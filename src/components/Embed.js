be6fd465ef3ca866d380d718c8ec1764eb49afcb
Max Kohler <hi@maxkoehler.com>
2022-12-05 10:44:44 +0100

    Closes #11

--- src/components/Embed.js
+++ src/components/Embed.js
@@ -1,12 +1,13 @@
 import React from "react";
 import * as styles from "./Embed.module.scss";
 
-function Embed({ src, size }) {
+function Embed({ src, size, caption }) {
   return (
     <figure class={`${styles.container} ${styles[size]}`}>
       <div class={styles.iframeContainer}>
         <iframe src={src} loading="lazy" allowFullScreen frameborder="0"></iframe>
       </div>
+      {caption !== "" && <figcaption className={styles.caption}>{caption}</figcaption>}
     </figure>
   );
 }
